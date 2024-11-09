920c17e84adf7a611b6abddad1a8d0ea792ed326
keely-cooley <keelymacloud@yahoo.com>
2024-11-09 12:14:31 -0600

    initial commit - connected to MySQL database

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+// const cors = require("cors");
+const app = express();
+const port = 8083;
+require("dotenv").config();
+
+let dbConnect = require('./dbConnect')
+
+// const corsOptions = {
+//   origin: "http://localhost:5173",
+// };
+
+// app.use(cors(corsOptions));
+
+// const authRoutes = require("./routes/authRoutes");
+// const userPostRoutes = require("./routes/userPostRoutes");
+// const movieRoutes = require("./routes/movieroutes");
+// const landingPostRoutes = require("./routes/landingPostRoutes");
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// app.use("/", express.static("public"));
+app.use(express.json());
+
+// //ROUTES
+// //http://localhost:8083/
+// app.use("/", authRoutes);
+// //http:localhost:8083/userPosts
+// app.use("/userPosts", userPostRoutes);
+// //http://localhost:8083/movies
+// //landing page movie array only
+// app.use("/movies", movieRoutes);
+// //https://localhost:8083/landingPost
+// //landing page posts only
+// app.use("/landingPost", landingPostRoutes);
+
+app.listen(port, () => {
+  console.log(`Cinnefiles listening at http://localhost:${port}`);
+});
